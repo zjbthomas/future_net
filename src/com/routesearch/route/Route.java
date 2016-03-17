170fe38d58b06675e7c7c4000de0969a12d3c365
Ollie Chen <cxcollie@gmail.com>
2016-03-17 17:08:53 +0800

    Update search PathID
    1. Use method searchPathID(int src, int dest) to search the PathID with
    src and des provided.
    2. Test example in main.java

--- src/com/routesearch/route/Route.java
+++ src/com/routesearch/route/Route.java
@@ -26,17 +26,20 @@ public final class Route
 	private static final int PATHDEST = 2;
 	private static final int COST = 3;
 	
+	private static HashMap<PathKey, Integer[]> edges1 = new HashMap<PathKey, Integer[]>();
+	
     public static String searchRoute(String graphContent, String condition)
     {
     	
-    	// Handle graphContent
+    	/*// Handle graphContent
     	HashMap<Integer, Integer[]> edges = new HashMap<Integer, Integer[]>();
     	for (String edge : graphContent.split("\n")) {
     		String[] edgeSplit = edge.split(",");
     		edges.put(Integer.parseInt(edgeSplit[PATHID]), new Integer[]{Integer.parseInt(edgeSplit[PATHSRC]),
     																	 Integer.parseInt(edgeSplit[PATHDEST]),
     																	 Integer.parseInt(edgeSplit[COST])});
-    	}
+    	}*/
+
     	// Handle condition
     	String[] conditionSplit = condition.split(",|\n");
     	Integer sourceID = Integer.parseInt(conditionSplit[0]);
@@ -47,6 +50,31 @@ public final class Route
     	}
     	
         return "hello world!";
+    }
+    
+    public static String setrout(String graphContent) {
+    	for (String edge : graphContent.split("\n")) {
+    		String[] edgeSplit = edge.split(",");
+    		PathKey key1=new PathKey(Integer.parseInt(edgeSplit[PATHSRC]), Integer.parseInt(edgeSplit[PATHDEST]));
+    		// Key of PathKey type
+    		if (edges1.containsKey(key1)) {
+    			if (edges1.get(key1)[1] <= Integer.parseInt(edgeSplit[COST])) {
+    				continue; // skip if the old path has smaller cost
+    			}
+    		}
+    		edges1.put(key1, 
+    				   new Integer[]{Integer.parseInt(edgeSplit[PATHID]),Integer.parseInt(edgeSplit[COST])});
+    		// put new path or update old path (with same src & des)
+    	}
+    	return "hello";
+    }
+    
+    public static int searchPathID(Integer src, Integer dest) {
+    	PathKey key1=new PathKey(src,dest);
+    	if (edges1.containsKey(key1)) {
+    		return edges1.get(key1)[0]; // return PathID
+    	}
+    	return 10000; // exception
     }
     
     /*
@@ -68,4 +96,35 @@ public final class Route
     	return ret;
     }
     */
+    
+    
 }
+
+class PathKey { // overwrite the Key element (cxc)
+    public final int pathsrc;
+    public final int pathdes;
+
+    public PathKey(int src, int des) {
+        pathsrc = src;
+        pathdes = des;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (! (object instanceof PathKey)) {
+            return false;
+        }
+
+        PathKey otherKey = (PathKey) object;
+        return (this.pathsrc == otherKey.pathsrc) && (this.pathdes == otherKey.pathdes);
+    }
+
+    @Override
+    public int hashCode() {
+    	int result = 0; // any prime number
+    	result = result + pathsrc;
+    	result =  600 * result + pathdes;
+    	return result;
+    }
+}
+
